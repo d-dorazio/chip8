71540b9a9cc8a337bc8d4f875a19bb09925ec2a3
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-01-18 23:31:00 +0100

    start chip-web frontend

---
+++ chip8-web/crate/src/lib.rs
@@ -0,0 +1,108 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use cfg_if::cfg_if;
+
+use wasm_bindgen;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+
+cfg_if! {
+    // When the `console_error_panic_hook` feature is enabled, we can call the
+    // `set_panic_hook` function to get better error messages if we ever panic.
+    if #[cfg(feature = "console_error_panic_hook")] {
+        use console_error_panic_hook::set_once as set_panic_hook;
+    } else {
+        #[inline]
+        fn set_panic_hook() {}
+    }
+}
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+const MAZE: &[u8] = include_bytes!("../../../games/MAZE");
+const FREQ: usize = 500;
+
+#[wasm_bindgen]
+pub fn run() -> Result<(), JsValue> {
+    set_panic_hook();
+
+    let document = window().document().expect("should have a Document");
+    let body = document.body().unwrap();
+
+    let canvas = document
+        .create_element("canvas")?
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+    canvas.set_width(640);
+    canvas.set_height(320);
+
+    body.append_child(&canvas)?;
+
+    let mut chip8 = chip8::Chip8::with_program(rand::rngs::OsRng::new().unwrap(), MAZE).unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.fill_rect(0.0, 0.0, 640.0, 320.0);
+
+    register_animation_frame_loop(move || {
+        // TODO: handle input
+
+        for _ in 0..FREQ / 60 {
+            chip8.emulate_cycle();
+        }
+
+        for (y, x, p) in chip8.pixels() {
+            let style = if *p == 1 {
+                JsValue::from_str("white")
+            } else {
+                JsValue::from_str("black")
+            };
+
+            context.set_fill_style(&style);
+
+            context.fill_rect(x as f64 * 10.0, y as f64 * 10.0, 10.0, 10.0);
+        }
+
+        // TODO: audio
+
+        chip8.decrease_timers();
+    });
+
+    Ok(())
+}
+
+fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+fn register_animation_frame_loop<F: FnMut() + 'static>(mut fun: F) {
+    let f = Rc::new(RefCell::new(None));
+    let g = f.clone();
+
+    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+        fun();
+
+        request_animation_frame(f.borrow().as_ref().unwrap());
+    }) as Box<FnMut()>));
+
+    request_animation_frame(g.borrow().as_ref().unwrap());
+}
+
+fn request_animation_frame(f: &Closure<FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
