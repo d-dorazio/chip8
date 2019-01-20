42a5a6dc561ffc3b53a88390c15f00d49ae58238
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-01-21 00:31:49 +0100

    chip8-web: extract css in single file

--- chip8-web/webpack.config.js
+++ chip8-web/webpack.config.js
@@ -1,31 +1,58 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const dist = path.resolve(__dirname, "dist");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
-  entry: "./js/index.js",
+  entry: { main: "./js/index.js" },
+
   output: {
     path: dist,
-    filename: "bundle.js"
+    filename: "bundle.js",
+    chunkFilename: '[name].bundle.js',
   },
-  module: {
-    rules: [{
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }]
-  },
+
   devServer: {
     contentBase: dist,
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html'
     }),
 
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+
     new WasmPackPlugin({
       crateDirectory: path.resolve(__dirname, "crate")
     }),
-  ]
-};
+  ],
+
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        "style-loader",
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+      ]
+    }]
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
+}
