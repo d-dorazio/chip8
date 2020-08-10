1922bfcb1fbad0eef4294eb7b56ccc0d0e95dfb1
Daniele D'Orazio <d.dorazio96@gmail.com>
2020-08-10 15:45:05 +0200

    chip8: npm update

--- chip8-web/webpack.config.js
+++ chip8-web/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const dist = path.resolve(__dirname, "dist");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
@@ -20,7 +20,7 @@ module.exports = {
   },
 
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
 
     new HtmlWebpackPlugin({
       template: 'index.html'
